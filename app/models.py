11d19f586e5ed5cc8abdbfbe8faf6f5d4fa081bc
Manish <mdashsharma95@gmail.com>
2023-06-23 12:29:35 +0530

    Removed UNIQUE field in voucher code

--- app/models.py
+++ app/models.py
@@ -48,7 +48,7 @@ class Voucher(models.Model):
 class RedeemVoucher(models.Model):
     email = models.CharField(max_length=255,unique=True)
     name = models.CharField(max_length=255,unique=True)
-    voucher_code = models.CharField(max_length=255,unique=True)
+    voucher_code = models.CharField(max_length=255)
 
     def __str__(self):
         return str(self.name)
