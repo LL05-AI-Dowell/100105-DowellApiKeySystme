f1afd9a99b7dd85bd5c9b0a72b494429342265f2
fraolb <Fraolbereket@gmail.com>
2023-07-03 13:11:20 +0300

    under construction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import CreateApi from "./pages/CreateApi";
 import CreatedApiKeys from "./pages/CreatedApiKeys";
 import Profile from "./pages/Profile";
+import ConstructionPage from "./pages/ConstructionPage";
 import useDowellLogin from "./hooks/useDowellLogin";
 import { useUserContext } from "./contexts/UserContext";
 
@@ -20,8 +21,8 @@ function App() {
   )
 
   return (
-    <Routes path='/' element={<CreateApi />} >
-      <Route index element={<CreateApi />} />
+    <Routes path='/' element={<ConstructionPage />} >
+      <Route index element={<ConstructionPage />} />
       <Route path='/profile' element={<Profile />} />
       <Route path='/createdapikeys' element={<CreatedApiKeys />} />
     </Routes>
